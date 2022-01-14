a03b85a0d995bf356b57fb9c1c2dd44dd34dddcd
VAWESOME005 <40074540+VAWESOME005@users.noreply.github.com>
2022-01-13 19:02:02 -0800

    Add files via upload

---
+++ third_inter.py
@@ -0,0 +1,80 @@
+import pandas as pd
+import plotly.figure_factory as ff
+import plotly.graph_objects as go
+import statistics as stats
+import random
+
+df = pd.read_csv('population.csv')
+
+population = df["Math_score"].tolist()
+
+og_mean = stats.mean(population)
+og_stdev = stats.stdev(population)
+
+print(og_mean, og_stdev)
+
+
+
+def random_sampling(counter):
+    dataset = []
+
+    for i in range(0, counter):
+        random_index = random.randint(0, len(population) - 1)
+        value = population[random_index]
+        dataset.append(value)
+     
+    mean = stats.mean(dataset)
+    return mean
+
+mean_list = []
+
+for i in range(0,1000):
+    mean_values = random_sampling(100)
+    mean_list.append(mean_values)
+
+sampling_stdev = stats.stdev(mean_list)
+sampling_mean = stats.mean(mean_list)
+print(sampling_mean, sampling_stdev)
+
+
+
+fig = ff.create_distplot([mean_list],["Math Scores"], show_hist = False)
+fig.add_trace(go.Scatter(x = [sampling_mean, sampling_mean], y = [0,0.3], mode = "lines", name = "MEAN"))
+#fig.show()
+
+stdev1_start = sampling_mean - sampling_stdev
+stdev1_end = sampling_mean + sampling_stdev
+
+stdev2_start = sampling_mean - (2*sampling_stdev)
+stdev2_end = sampling_mean + (2*sampling_stdev)
+
+stdev3_start = sampling_mean - (3*sampling_stdev)
+stdev3_end = sampling_mean + (3*sampling_stdev)
+
+fig.add_trace(go.Scatter(x = [stdev1_start, stdev1_start], y = [0, 0.3], mode = 'lines', name = "Standard Deviation #1 Start"))
+fig.add_trace(go.Scatter(x = [stdev1_end, stdev1_end], y = [0, 0.3], mode = 'lines', name = "Standard Deviation #1 End"))
+
+fig.add_trace(go.Scatter(x = [stdev2_start, stdev2_start], y = [0, 0.3], mode = 'lines', name = "Standard Deviation #2 Start"))
+fig.add_trace(go.Scatter(x = [stdev2_end, stdev2_end], y = [0, 0.3], mode = 'lines', name = "Standard Deviation #2 End"))
+
+fig.add_trace(go.Scatter(x = [stdev3_start, stdev3_start], y = [0, 0.3], mode = 'lines', name = "Standard Deviation #3 Start"))
+fig.add_trace(go.Scatter(x = [stdev3_end, stdev3_end], y = [0, 0.3], mode = 'lines', name = "Standard Deviation #3 End"))
+
+#fig.show()
+
+
+
+
+df3 = pd.read_csv("intervention_3.csv")
+
+population3 = df3["Math_score"].tolist()
+
+intervention3_mean = stats.mean(population3)
+
+fig.add_trace(go.Scatter(x = [intervention3_mean, intervention3_mean], y = [0, 0.3], mode = "lines", name = "Intervention 3 Average"))
+
+fig.show()
+
+z_score = (intervention3_mean - sampling_mean) / sampling_stdev
+
+print("Z SCORE: ", z_score)
